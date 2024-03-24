6acb09d76138cbe8253e313e2f530ae8941043f3
Evg. <gruppa102m@yandex.ru>
2024-03-24 03:05:01 +0300

    fix: при обновлении пользователя добавить проверку на пустое имя

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.controller.group.CreateGroup;
 import ru.yandex.practicum.filmorate.controller.group.UpdateGroup;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.model.User.UserBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,32 +24,36 @@ public class UserController {
 
     @GetMapping()
     public List<User> getUsers() {
-        log.info("Количество пользователей = " + users.size());
+        log.info("Количество пользователей = {}", users.size());
         return new ArrayList<>(users.values());
     }
 
     @PostMapping()
     public User addUser(@Validated(CreateGroup.class) @RequestBody User user) {
-        UserBuilder userBuilder = user.toBuilder();
-        User newUser = userBuilder.id(idCounter++).build();
+        user.setId(idCounter++);
         String name = user.getName();
         if (name == null || name.isBlank()) {
-            log.info("У пользователя не задано имя, вместо имени будет использоваться логин = " + newUser.getLogin());
-            newUser = userBuilder.name(user.getLogin()).build();
+            log.info("У пользователя не задано имя, вместо имени будет использоваться логин = {}", user.getLogin());
+            user.setName(user.getLogin());
         }
-        users.put(newUser.getId(), newUser);
-        log.info("Пользователь с id = " + newUser.getId() + " добавлен");
-        return newUser;
+        users.put(user.getId(), user);
+        log.info("Пользователь с id = {} добавлен", user.getId());
+        return user;
     }
 
     @PutMapping()
     public User updateUser(@Validated(UpdateGroup.class) @RequestBody User user) {
         if (users.get(user.getId()) == null) {
-            log.warn("Пользователя с id = " + user.getId() + " не существует");
+            log.warn("Пользователя с id = {} не существует", user.getId());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Пользователя с id = " + user.getId() + " не существует");
         }
+        String name = user.getName();
+        if (name == null || name.isBlank()) {
+            log.info("У пользователя не задано имя, вместо имени будет использоваться логин = {}", user.getLogin());
+            user.setName(user.getLogin());
+        }
         users.put(user.getId(), user);
-        log.info("Пользователь с id = " + user.getId() + " обновлён");
+        log.info("Пользователь с id = {} обновлён", user.getId());
         return user;
     }
 }
